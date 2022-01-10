becdd96a5b8ab29d70c7c723861f7f5ec10b674f
ikeyu0806 <ikeyu0806@gmail.com>
2022-01-10 16:49:32 +0900

    generate enemy

--- Assets/Scripts/AttackerController.cs
+++ Assets/Scripts/AttackerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AttackerController : MonoBehaviour
 {
+    int unitHP = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,20 @@ public class AttackerController : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(1f, 0, 0) * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    public void TakeHit(float damage)
+    {
+        unitHP = (int)Mathf.Clamp(unitHP - damage, 0, unitHP);
+
+        if (unitHP <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public float timeOut;
-    private float timeElapsed;
-    public GameObject attacker;
+    private float generatePlayerUnitTime;
+    private float generateEnemyUnitTime;
+    public GameObject playerUnit;
+    public GameObject enemyUnit;
     public GameObject castle;
 
     // Start is called before the first frame update
@@ -18,13 +20,22 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeElapsed += Time.deltaTime;
+        generatePlayerUnitTime += Time.deltaTime;
 
-        if (timeElapsed >= timeOut)
+        if (generatePlayerUnitTime >= timeOut)
         {
-            Instantiate(attacker, new Vector3(castle.transform.position.x, castle.transform.position.y, castle.transform.position.z), Quaternion.identity);
+            Instantiate(playerUnit, new Vector3(castle.transform.position.x, castle.transform.position.y, castle.transform.position.z), Quaternion.identity);
 
-            timeElapsed = 0.0f;
+            generatePlayerUnitTime = 0.0f;
+        }
+
+        generateEnemyUnitTime += Time.deltaTime;
+
+        if (generateEnemyUnitTime >= timeOut)
+        {
+            Instantiate(enemyUnit, new Vector3(10f, castle.transform.position.y, 0), Quaternion.identity);
+
+            generateEnemyUnitTime = 0.0f;
         }
     }
 }
